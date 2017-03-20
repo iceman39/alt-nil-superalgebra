8283f89c3fd97591d3f269a1964815729a270d6d
Matyáš Hollmann <Maty@hollmmat_ntb.wifi.nat>
2017-03-20 21:27:22 +0100

    Initial Commit

---
+++ BP_multiplication/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BP_multiplication
+//
+//  Created by Matyáš Hollmann on 20/03/2017.
+//  Copyright © 2017 Matyáš Hollmann. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
